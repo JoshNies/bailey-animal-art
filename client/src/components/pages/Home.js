7a0c076c74744cf0425ee9302003f4d8f282d06b
Josh Nies <whatsthatfunction@gmail.com>
2019-05-02 18:24:29 -0700

    init commit

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from './../../logo.svg'
+
+class Home extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {/* Example link user react-router-dom */}
+          <Link to={'./about'}>
+            <button variant="raised">
+                Ooh next page please!
+            </button>
+          </Link>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Home;
