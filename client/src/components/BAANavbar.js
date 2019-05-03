4925d35cd3508746d5f49ad28be97107d3d6fe33
Josh Nies <whatsthatfunction@gmail.com>
2019-05-03 08:36:42 -0700

    Navbar

---
+++ client/src/components/BAANavbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from 'react-bulma-components/lib/components/navbar'
+
+class BAANavbar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      open: false
+    }
+  }
+
+  burgerClicked() {
+    this.setState({
+      open: !this.state.open
+    })
+  }
+
+  render () {
+    return (
+      <Navbar
+        color="white"
+        fixed="top"
+        active={this.state.open}
+      >
+        <Navbar.Brand>
+          <Navbar.Item renderAs="a" href="#">Bailey Animal Art</Navbar.Item>
+          <Navbar.Burger
+            active={this.state.open.toString()}
+            onClick={this.burgerClicked.bind(this)}
+          />
+        </Navbar.Brand>
+        <Navbar.Menu active={this.state.open.toString()}>
+          <Navbar.Container position="end">
+            <Navbar.Item href="#">custom order</Navbar.Item>
+            <Navbar.Item href="#">shop</Navbar.Item>
+            <Navbar.Item href="#">about</Navbar.Item>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+    )
+  }
+}
+
+export default BAANavbar;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Button from 'react-bulma-components/lib/components/button'
+//import Button from 'react-bulma-components/lib/components/button'
+import BAANavbar from '../BAANavbar'
 
 class Home extends Component {
   render () {
     return (
       <div className="home">
-        <h1>Home page</h1>
-        <Button color="primary">My first button!</Button>
+        <BAANavbar/>
       </div>
     )
   }
