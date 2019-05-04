fd8046e7a813024729d3e2ae894b2e668d908332
Josh Nies <whatsthatfunction@gmail.com>
2019-05-03 19:05:07 -0700

    Misc

--- client/src/components/BAAFooter/BAAFooter.js
+++ client/src/components/BAAFooter/BAAFooter.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Footer from 'react-bulma-components/lib/components/footer'
 
 class BAAFooter extends Component {
   render () {
     return (
       <div className="baa-footer">
         <p>Copyright © 2019 Bailey Animal Art.  All Rights Reserved.</p>
-        <p>Website by Joshua Nies</p>
       </div>
     )
   }
