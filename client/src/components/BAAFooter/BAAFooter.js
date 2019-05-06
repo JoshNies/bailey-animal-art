276993222eeeead4f959588f1a855252528184e2
Josh Nies <whatsthatfunction@gmail.com>
2019-05-06 15:30:26 -0700

    Footer paw print image

--- client/src/components/BAAFooter/BAAFooter.js
+++ client/src/components/BAAFooter/BAAFooter.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Img from 'react-image'
+import PawImage from './BaileyPaw.png'
 
 class BAAFooter extends Component {
   render () {
     return (
       <div className="baa-footer">
+        <Img src={PawImage} alt="Bailey Animal Art"/>
         <p>Copyright © 2019 Bailey Animal Art.  All Rights Reserved.</p>
       </div>
     )
