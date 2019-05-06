478bd1336ac67732431f41e7a90c87f583ecc087
Josh Nies <whatsthatfunction@gmail.com>
2019-05-06 08:30:51 -0700

    Nodemailer

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// (Example endpoint) An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
+// Send custom order email
+app.get('/api/custom-order/send', (req, res) => {
+  console.log("Sending email...");
+
+  // Create nodemailer transporter
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  // Create mail options
+  let mailOptions = {
+    from: '"Bailey Animal Art" <baileyanimalartbaa@gmail.com>',
+    to: 'whatsthatfunction@gmail.com',
+    subject: 'Bailey Animal Art - Custom Order',
+    text: 'Test!'
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions)
+    .then(res => {
+      console.log("Email sent! Response: ", res)
+    })
+    .catch(e => {
+      console.log("Email error: ", e)
+    })
 });
 
 // Handles any requests that don't match the ones above
